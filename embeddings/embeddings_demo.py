caac9add1ceb0d4c6d3e530a8dbd0013f770511f
Gowtham867 <maruboinagowtham@gmail.com>
2026-01-29 14:39:26 +0530

    Delete embeddings directory

--- embeddings/embeddings_demo.py
+++
@@ -1,5 +0,0 @@
-from langchain_ollama import OllamaEmbeddings
-llm=OllamaEmbeddings(model="nomic-embed-text")  #mxbai-embed-large    snowflake-arctic-embed
-qn=input("Enter  the qn")
-response=llm.embed_query(qn)
-print(response)
